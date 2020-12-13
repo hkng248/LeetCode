fbd65c05e083748b15fa72564b82e5d57e44c520
haitai <hkng248@gmail.com>
2020-12-13 16:35:05 -0500

    LC

--- 122_Best_Time_To_Buy_And_Sell_Stock_II.java
+++ 122_Best_Time_To_Buy_And_Sell_Stock_II.java
@@ -1,8 +1,12 @@
-// O(n) space | O(1) speed 
-public int maxProfit(int[] prices) {
-        int profit = 0; 
-        for(int i = 1; i < prices.length; i++)
-        {
-            profit += Math.max(0, prices[i] - prices[i - 1]); 
-        }
-        return prof
+class Solution {
+    // O(n) space | O(1) speed 
+    public int maxProfit(int[] prices) {
+            int profit = 0; 
+            for(int i = 1; i < prices.length; i++)
+            {
+                profit += Math.max(0, prices[i] - prices[i - 1]); 
+            }
+            return profit;
+            }
+    
+    }
