6639c9aa1b71ef06fe923a978b4ec683e94dc281
haitai <hkng248@gmail.com>
2021-01-12 14:33:39 -0500

    LC

--- 49_Group_Anagrams.py
+++ 49_Group_Anagrams.py
@@ -1,3 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagramMap = {} 
+        for i in strs: 
+            iSort = ''.join(sorted(i))
+            if iSort in anagramMap.keys():
+                anagramMap[iSort].append(i)
+            else:
+                anagramMap[iSort] = [i]
+        return list(anagramMap.values())
+        
+
+
+
+'''
 # Python 3.6.5 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
@@ -14,5 +29,6 @@ class Solution:
         for k in mapOfLetters.values():
             mapOfWords.append(k)
         return mapOfWords 
+ '''
             
         
