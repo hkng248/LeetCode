4f60817116c4a2b77b9178b3c0da62028f588e07
haitai <hkng248@gmail.com>
2021-03-04 21:51:53 -0500

    LC

--- 268_Missing_Number.py
+++ 268_Missing_Number.py
@@ -1,7 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        missing = len(nums)
-        for i, num in enumerate(nums):
-            missing ^=i ^ num 
-        return missing 
+        e = len(nums) * (len(nums) + 1) // 2 
+        a = sum(nums)
+        return e - a 
         
--- 895_Maximum_Frequency_Stack.py
+++ 895_Maximum_Frequency_Stack.py
@@ -1,44 +1,71 @@
-### TLE (This solution does not work)
 
 class FreqStack:
-    e = None 
-    eFreq = None 
-    mF = None 
+    mf = None 
+    f = None 
+    fs = None 
     
     def __init__(self):
-        self.e = [] 
-        self.eFreq = {} 
         self.mf = 0 
-        
+        self.f = defaultdict(int) 
+        self.fs = defaultdict(list) 
+
     def push(self, x: int) -> None:
-        self.e.append(x)
-        if x in self.eFreq.keys():
-            self.eFreq[x] += 1 
-        else:
-            self.eFreq[x] = 1 
-        self.mf = max(self.eFreq[x], self.mf)
-        
-    def find_last(self, lst, elm):
-        gen = (len(lst) - 1 - i for i, v in enumerate(reversed(lst)) if v == elm)
-        return next(gen, None)        
+        self.f[x] += 1 
+        self.mf = max(self.mf, self.f[x]) 
+        self.fs[self.f[x]].append(x)
         
     def pop(self) -> int:
-        #find value with most frequent occurrence 
-        x = [k for k,v in self.eFreq.items() if v == self.mf]
-        #find which value is closest to the top
-        maxI = 0
-        for i in x: 
-            maxI = max(maxI, self.find_last(self.e, i))
-        v = self.e[maxI]
-        self.eFreq[v] -= 1
-        del self.e[maxI] 
-        
-        if len(x) == 1:
+        c = self.fs[self.mf][-1] 
+
+        self.fs[self.mf].pop() 
+        self.f[c] -= 1 
+
+        if len(self.fs[self.mf]) <= 0: 
+            del self.fs[self.mf] 
             self.mf -= 1 
-        return v 
-            
-            
+        return c 
+
 # Your FreqStack object will be instantiated and called as such:
 # obj = FreqStack()
 # obj.push(x)
 # param_2 = obj.pop()
+
+
+### TLE (This solution does not work)
+
+# class FreqStack:
+#     e = None 
+#     eFreq = None 
+#     mF = None 
+    
+#     def __init__(self):
+#         self.e = [] 
+#         self.eFreq = {} 
+#         self.mf = 0 
+        
+#     def push(self, x: int) -> None:
+#         self.e.append(x)
+#         if x in self.eFreq.keys():
+#             self.eFreq[x] += 1 
+#         else:
+#             self.eFreq[x] = 1 
+#         self.mf = max(self.eFreq[x], self.mf)
+        
+#     def find_last(self, lst, elm):
+#         gen = (len(lst) - 1 - i for i, v in enumerate(reversed(lst)) if v == elm)
+#         return next(gen, None)        
+        
+#     def pop(self) -> int:
+#         #find value with most frequent occurrence 
+#         x = [k for k,v in self.eFreq.items() if v == self.mf]
+#         #find which value is closest to the top
+#         maxI = 0
+#         for i in x: 
+#             maxI = max(maxI, self.find_last(self.e, i))
+#         v = self.e[maxI]
+#         self.eFreq[v] -= 1
+#         del self.e[maxI] 
+        
+#         if len(x) == 1:
+#             self.mf -= 1 
+#         return v 
